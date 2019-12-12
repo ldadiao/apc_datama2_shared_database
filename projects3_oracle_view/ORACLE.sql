74909d69dbfdc88d9061050fc82f1f332d5f85e9
ldadiao <52308797+ldadiao@users.noreply.github.com>
2019-12-12 14:29:45 +0800

    Add files via upload

---
+++ projects3_oracle_view/ORACLE.sql
@@ -0,0 +1,40 @@
+CREATE TABLE MI182_LDADIAO.department (id INT NOT NULL PRIMARY KEY, codename VARCHAR(45));
+
+INSERT INTO DEPARTMENT
+	VALUES(1, 'TRP-QC');
+INSERT INTO department
+	VALUES(2, 'SUJI-TOSHI');
+INSERT INTO department
+	VALUES(3, 'PPAP');
+INSERT INTO department
+	VALUES(4, 'TRP-IN HOUSE');
+	
+CREATE TABLE supplier (id INT NOT NULL PRIMARY KEY, department_id INT, 
+	FOREIGN KEY (department_id) REFERENCES department(id));
+    
+INSERT INTO supplier
+	VALUES(1, 2);
+    
+INSERT INTO supplier
+	VALUES(2, 3);
+    
+INSERT INTO supplier
+	VALUES(3, 4);
+    
+INSERT INTO supplier
+	VALUES(4, 1);
+    
+INSERT INTO supplier
+	VALUES(5, 2);
+    
+ALTER USER MI182_LDADIAO quota unlimited on USERS;
+
+CREATE VIEW department_details
+as
+SELECT
+	id,
+	codename
+from
+	supplier
+inner join
+	department using (id);
